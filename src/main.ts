07b5c3d20820c9a7893a4100b46d2d9080a4bec3
Aline Fernandes <alinerfernandes55@gmail.com>
2026-06-15 22:54:42 -0300

    configura express-session

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import session from 'express-session';
 import { Liquid } from 'liquidjs';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET ?? 'segredo',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 8,
+      },
+    }),
+  );
 
   const viewsPath = join(__dirname, '..', 'views');
   const engine = new Liquid({

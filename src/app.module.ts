459c9cfa952603b9912b68a20592eea2fb164c7e
Aline Fernandes <alinerfernandes55@gmail.com>
2026-06-15 22:04:14 -0300

    configura typeorm com sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: process.env.DATABASE_PATH ?? 'data/expedicao.sqlite',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
